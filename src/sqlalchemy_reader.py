1aacd3463e82ac8564d9fcbd6093155351dc0b05
Y-Suzaki <geranium04.24.1981@gmail.com>
2018-06-11 07:58:28 +0900

    InnerJoinとOuterJoinを明示的に指定するように修正

--- src/sqlalchemy_reader.py
+++ src/sqlalchemy_reader.py
@@ -3,6 +3,8 @@ from model.skill import Skill
 from model.engineer import Engineer
 from model.engineer import EngineerSkill
 from sqlalchemy.orm import eagerload
+from sqlalchemy.orm import joinedload
+from sqlalchemy import asc
 
 
 class SqlAlchemyReader:
@@ -14,12 +16,27 @@ class SqlAlchemyReader:
     @staticmethod
     def get_skill_all(limit=10):
         with SessionContext() as session:
-            return session.query(Skill).limit(limit).all()
+            return session.query(Skill)\
+                .limit(limit).all()
+
+    @staticmethod
+    def get_skill_all_order_by_name(limit=10):
+        with SessionContext() as session:
+            return session.query(Skill)\
+                .order_by(asc(Skill.name))\
+                .limit(limit).all()
 
     @staticmethod
     def get_skill_by_name(name):
         with SessionContext() as session:
-            return session.query(Skill).filter(Skill.name == name).all()
+            return session.query(Skill)\
+                .filter(Skill.name == name).all()
+
+    @staticmethod
+    def get_skill_by_names(names):
+        with SessionContext() as session:
+            return session.query(Skill)\
+                .filter(Skill.name.in_(names)).all()
 
     @staticmethod
     def get_engineer_by_id(id):
@@ -27,7 +44,33 @@ class SqlAlchemyReader:
             return session.query(Skill).get(id)
 
     @staticmethod
-    def get_engineer_with_skill(engineer_id):
+    def get_engineer_with_skill_by_id(engineer_id):
         with SessionContext() as session:
-            return session.query(Engineer).options(eagerload('engineer_skill'), eagerload('engineer_skill.skill'))\
+            return session.query(Engineer) \
+                .options(joinedload(Engineer.engineer_skill).joinedload(EngineerSkill.skill))\
                 .get(engineer_id)
+
+    @staticmethod
+    def get_engineer_with_skill_all():
+        with SessionContext() as session:
+            return session.query(Engineer)\
+                .join(EngineerSkill)\
+                .join(Skill) \
+                .options(joinedload(Engineer.engineer_skill).joinedload(EngineerSkill.skill)).all()
+
+    @staticmethod
+    def get_engineer_with_skill_by_skill_name(skill_name):
+        with SessionContext() as session:
+            return session.query(Engineer)\
+                .join(EngineerSkill)\
+                .join(Skill)\
+                .filter(Skill.name == skill_name)\
+                .options(joinedload(Engineer.engineer_skill).joinedload(EngineerSkill.skill)).all()
+
+    @staticmethod
+    def get_engineer_with_skill_all_outer_join():
+        with SessionContext() as session:
+            return session.query(Engineer)\
+                .outerjoin(EngineerSkill) \
+                .outerjoin(Skill) \
+                .options(joinedload(Engineer.engineer_skill).joinedload(EngineerSkill.skill)).all()
--- src/sqlalchemy_reader_test.py
+++ src/sqlalchemy_reader_test.py
@@ -8,19 +8,32 @@ class SqlAlchemyReaderTest(unittest.TestCase):
         skill = SqlAlchemyReader.get_skill_by_id('00001')
         print(skill)
 
-        skills = SqlAlchemyReader.get_skill_all()
-        for skill in skills:
+        for skill in SqlAlchemyReader.get_skill_all():
             print(skill)
 
-        skills = SqlAlchemyReader.get_skill_by_name(name='AWS')
-        for skill in skills:
+        for skill in SqlAlchemyReader.get_skill_all_order_by_name():
+            print(skill)
+
+        for skill in SqlAlchemyReader.get_skill_by_name(name='AWS'):
+            print(skill)
+
+        for skill in SqlAlchemyReader.get_skill_by_names(['java1.8', 'AWS']):
             print(skill)
 
     def test_engineer_all(self):
-        engineer = SqlAlchemyReader.get_engineer_with_skill('00001')
+        engineer = SqlAlchemyReader.get_engineer_with_skill_by_id('00001')
         print(engineer)
         for engineer_skill in engineer.engineer_skill:
             print(engineer_skill.skill.name)
+
+        for engineer in SqlAlchemyReader.get_engineer_with_skill_all():
+            print(engineer)
+
+        for engineer in SqlAlchemyReader.get_engineer_with_skill_by_skill_name('python2'):
+            print(engineer)
+
+        for engineer in SqlAlchemyReader.get_engineer_with_skill_all_outer_join():
+            print(engineer)
 
 
 if __name__ == "__main__":
